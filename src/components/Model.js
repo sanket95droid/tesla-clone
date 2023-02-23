d361125331dbf996c2f83946027390ff575ec383
sanket95droid <sanketzambre1@gmail.com>
2023-02-23 21:53:03 +0530

    day01 changes

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FiChevronDown } from "react-icons/fi";
 
 const Model = () => {
   return (
-    <div className="flex flex-col">
+    <div>
         <div className="absolute inset-x-0 text-center top-[15%] ">
-            <h1 className="text-[40px] font-bold text-[#171a20]">Model 3</h1>
+            <h1 className="text-4xl font-bold text-[#171a20]">Model 3</h1>
             <p className="text-[17px] text-[#171a20]">Leasing starting at $349/mo</p>
         </div>
 
-        <div className="flex flex-row text-sm font-bold justify-between">
-            <button className="bg-[#171a20]/80 text-white rounded py-3 px-7">Custom Order</button>
-            <button className="bg-white text-[#393c41]/70 rounded py-3 px-7">Demo Drive</button>
+        <div className="flex flex-row items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[11%] font-bold">
+            <button className="bg-[#171a20]/90 text-white w:96 lg:w-72 m-3 rounded py-3 px-7">Custom Order</button>
+            <button className="bg-white/70 text-[#393c41] w:96 lg:w-72 m-3 rounded py-3 px-7">Demo Drive</button>
+        </div >
+
+        <div className="flex justify-center text-3xl text-[#171a20] absolute inset-x-0 bottom-[3%] animate-bounce hover:cursor-pointer">
+            <FiChevronDown/>
         </div>
     </div>
   )
